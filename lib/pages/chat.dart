ecad973544ba954e47ead529f17b6534b4f874d0
Vlad <i@rseeed.ru>
2023-10-15 09:15:14 +0300

    Готово

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
-
-const String openaiApiKey = "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";
 
 class User {
   final String name;
@@ -13,12 +9,12 @@ class User {
 }
 
 class ChatApp extends StatelessWidget {
-  const ChatApp({super.key});
+  const ChatApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const ChatScreen(),
+      body: ChatScreen(),
       bottomNavigationBar: CurvedNavigationBar(
           backgroundColor: const Color(0xFFF3F7FA),
           color: const Color(0xFF122790),
@@ -43,7 +39,7 @@ class ChatApp extends StatelessWidget {
 }
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
+  const ChatScreen({Key? key}) : super(key: key);
 
   @override
   State createState() => ChatScreenState();
@@ -54,42 +50,45 @@ class ChatScreenState extends State<ChatScreen> {
   final TextEditingController textController = TextEditingController();
   final User currentUser = User('Me');
   final User otherUser = User('Other User');
+  int? replyToIndex;
 
-  Future<String> getChatGptResponce(String text) async {
-    final response = await http.post(
-      Uri.parse('https://api.openai.com/v1/engines/davinci-codex/completions'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $openaiApiKey',
-      },
-      body: jsonEncode({
-        'promt': text,
-        'max_tokens': 50, //Максимальное кол-во символов в ответе
-      }),
-    );
+  final Map<String, String> messageResponses = {
+    'привет': 'Привет, как я могу помочь?',
+    'как дела?': 'У меня все хорошо, спасибо!',
+    'что делаешь?': 'Я помогаю вам! Как я могу помочь?',
+    'расскажи про ипотеку':
+        'У нас очень выгодные предложения по ипотеке) Хочешь расскажу подробнее?',
+    'кто твои разработчики':
+        'Меня создала команда CodeCat, я готов помогать тебе с любыми вопросам, не стесняйся, задавай.',
+  };
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return data['choices'][0]['text'];
-    } else {
-      throw Exception('Failed to get responce from ChatGpt');
+  void _handleSubmitted(String text, User user) {
+    if (text.isNotEmpty) {
+      setState(() {
+        String response = messageResponses[text.toLowerCase()] ??
+            "Извините, не могу понять ваш запрос.";
+
+        if (replyToIndex == null) {
+          messages.insert(0, {'text': response, 'user': otherUser});
+          messages.insert(1, {'text': text, 'user': user});
+        } else {
+          messages.insert(replyToIndex! + 1, {'text': text, 'user': user});
+          messages
+              .insert(replyToIndex! + 2, {'text': response, 'user': otherUser});
+          replyToIndex = null;
+        }
+        textController.clear();
+      });
     }
   }
 
-  void _handleSubmitted(String text, User user) async {
-    if (text.isNotEmpty) {
-      setState(() {
-        messages.insert(0, {'text': text, 'user': user});
-        textController.clear();
-      });
-      // Добавляем ответ пользователя
-      if (user == currentUser) {
-        final response = await getChatGptResponce(text);
-        setState(() {
-          messages.insert(0, {'text': response, 'user': otherUser});
-        });
-      }
-    }
+  void setReplyTo(int index) {
+    setState(() {
+      replyToIndex = index;
+      textController.text = "Reply to: ${messages[index]['text']}";
+      textController.selection =
+          TextSelection.collapsed(offset: textController.text.length);
+    });
   }
 
   @override
@@ -105,6 +104,8 @@ class ChatScreenState extends State<ChatScreen> {
                 text: message['text'],
                 user: message['user'],
                 currentUser: currentUser,
+                index: index,
+                setReplyTo: setReplyTo,
               );
             },
             reverse: true,
@@ -148,12 +149,16 @@ class ChatMessage extends StatelessWidget {
   final String text;
   final User user;
   final User currentUser;
+  final int index;
+  final Function(int) setReplyTo;
 
   const ChatMessage(
       {super.key,
       required this.text,
       required this.user,
-      required this.currentUser});
+      required this.currentUser,
+      required this.index,
+      required this.setReplyTo});
 
   @override
   Widget build(BuildContext context) {
@@ -161,43 +166,38 @@ class ChatMessage extends StatelessWidget {
 
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        mainAxisAlignment:
-            isSentByMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      child: Column(
+        crossAxisAlignment:
+            isSentByMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: <Widget>[
-          isSentByMe
-              ? Container()
-              : CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  child: Text(user.name[0]),
-                ),
-          Container(
-            margin: isSentByMe
-                ? const EdgeInsets.only(left: 50.0)
-                : const EdgeInsets.only(right: 50.0),
-            padding: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              color: isSentByMe ? Colors.blue : Colors.grey[300],
-              borderRadius: isSentByMe
-                  ? const BorderRadius.only(
-                      topLeft: Radius.circular(10.0),
-                      bottomLeft: Radius.circular(10.0),
-                      topRight: Radius.circular(10.0),
-                    )
-                  : const BorderRadius.only(
-                      topRight: Radius.circular(10.0),
-                      bottomRight: Radius.circular(10.0),
-                      topLeft: Radius.circular(10.0),
-                    ),
+          GestureDetector(
+            onTap: () {
+              if (!isSentByMe) {
+                setReplyTo(index);
+              }
+            },
+            child: Container(
+              margin: isSentByMe
+                  ? const EdgeInsets.only(left: 50.0)
+                  : const EdgeInsets.only(right: 50.0),
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: isSentByMe ? Colors.blue : Colors.grey[300],
+                borderRadius: isSentByMe
+                    ? const BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        bottomLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                      )
+                    : const BorderRadius.only(
+                        topRight: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0),
+                        topLeft: Radius.circular(10.0),
+                      ),
+              ),
+              child: Text(text),
             ),
-            child: Text(text),
           ),
-          isSentByMe
-              ? CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  child: Text(currentUser.name[0]),
-                )
-              : Container(),
         ],
       ),
     );
